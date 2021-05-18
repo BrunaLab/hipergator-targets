d14ed646673f307816b38529bb5057dd962d950f
Aariq <scottericr@gmail.com>
2021-05-18 14:00:12 -0400

    tweaks to targets plan

--- _targets.R
+++ _targets.R
@@ -1,10 +1,10 @@
-## set options
+## Figures out if this is being run locally or on HiperGator:
 nodename <- Sys.info()["nodename"]
 if (grepl("ufhpc", nodename)) {
-  print("I know I am on the HiperGator!")
+  print("This is running on HiperGator!")
   options(clustermq.scheduler = "slurm", clustermq.template = "slurm_clustermq.tmpl")
 } else {
-  print("this is local")
+  print("This is running locally!")
   options(clustermq.scheduler="multiprocess")
 }
 
@@ -13,15 +13,14 @@ if (grepl("ufhpc", nodename)) {
 source("./packages.R")
 lapply(list.files("R", full.names = TRUE), source)
 
+## Set options
 tar_option_set()
 
 ## tar_plan supports drake-style targets and also tar_target()
 tar_plan(
   many_vects = make_vects(),
   means = map(many_vects, ~mean(.x)),
-  means_mean = mean(unlist(means)) #,
-  
-  # dat = gamSim(1, n = 25000, dist = "normal", scale = 20),
-  # model = fit_bam(dat)
-  
+  # these targets should be able to run in parallel:
+  means_mean = mean(unlist(means)),
+  means_sd = sd(unlist(means))
 )
